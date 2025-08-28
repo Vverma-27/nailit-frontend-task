6a56f10af1ce5d60848b7ea394cc83e86f20348c
Vihaan Verma <vverma270705@gmail.com>
2025-08-28 19:10:37 +0530

    add theme provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers/providers";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
@@ -25,14 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          {children}
+        <ThemeProvider>
+          <Providers>{children}</Providers>
           <Toaster />
-        </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
