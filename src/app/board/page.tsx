047220f80878887cb01d9140607b9f21922d0be2
Vihaan Verma <vverma270705@gmail.com>
2025-08-28 19:11:50 +0530

    header moved to layout remove from page,tsx

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { Header } from "@/components/layout/header";
 import { SprintBoard } from "@/components/board/sprint-board";
 
 export default function BoardPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <SprintBoard />
-    </div>
-  );
+  return <SprintBoard />;
 }
